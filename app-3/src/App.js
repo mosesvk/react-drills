31ba02e132ef43b300dc51ef65f4310767bca3db
Moses Kaumatule <kaumatule93@gmail.com>
2021-03-03 15:22:28 -0700

    complete app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      message: "",
+      foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+    };
+  }
+  
+  handleChange(filter) {
+    this.setState({ message: filter });
+  }
+
+  render() {
+    let foodsToDisplay = this.state.foods
+      .filter((element, index) => {
+        return element.includes(this.state.message);
+      })
+      .map((element, index) => {
+        return <h2 key={index}>{element}</h2>;
+      });
+
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text"/>
+        {foodsToDisplay}
+      </div>
+    );
+  }
 }
 
-export default App;
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       filterString: "",
+//       foods: ["spaghetti", "ice cream", "sushi", "bologna", "cheese"]
+//     };
+//   }
+
+//   handleChange(filter) {
+//     this.setState({ filterString: filter });
+//   }
+
+//   render() {
+//     let foodsToDisplay = this.state.foods
+//       .filter((element, index) => {
+//         return element.includes(this.state.filterString);
+//       })
+//       .map((element, index) => {
+//         return <h2 key={index}>{element}</h2>;
+//       });
+
+//     return (
+//       <div className="App">
+//         <input onChange={e => this.handleChange(e.target.value)} type="text" />
+//         {foodsToDisplay}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
